ea078f1f564295039c7a0e839cfb6bea5e1afe8b
Wren Morozova <siliconself@proton.me>
2024-01-06 21:29:49 -0700

    Added progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![doc = include_str!("../README.md")]
 
-use std::sync::Arc;
+use std::{sync::Arc, time::Duration};
 
 use clap::Parser;
 
@@ -11,9 +11,11 @@ mod gen;
 mod sites;
 
 use gen::SITES;
+use indicatif::ProgressBar;
 use once_cell::sync::OnceCell;
 use reqwest::Client;
 use sites::SiteType;
+use tokio::sync::mpsc::UnboundedSender;
 
 /// The shared client for Reqwest that all sites will use
 pub(crate) static REQWEST_CLIENT: OnceCell<Client> = OnceCell::new();
@@ -35,15 +37,16 @@ async fn test_username(
     name: Arc<String>,
     site_name: &str,
     site_data: &SiteType,
+    sender: UnboundedSender<Option<String>>
 ) {
-    match site_data.test(&name).await {
+    sender.send(match site_data.test(&name).await {
         Some(true) => {
-            println!("{site_name}: {name}");
+            Some(format!("{site_name}: {name}"))
         }
         Some(false) | None => {
-            println!("Not detected at {site_name}");
+            None
         }
-    }
+    });
 }
 
 #[tokio::main]
@@ -52,18 +55,25 @@ async fn main() {
     REQWEST_CLIENT
         .set(reqwest::Client::new())
         .expect("Cell cannot already be initialized");
-    let mut tasks = Vec::new();
+    let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
     for name in cli.names {
         let name_arc = Arc::new(name);
         for (site_name, site_data) in SITES.entries() {
             let new_arc = name_arc.clone();
-            let handle = tokio::task::spawn(test_username(
-                new_arc, site_name, site_data,
+            tokio::task::spawn(test_username(
+                new_arc, site_name, site_data, tx.clone()
             ));
-            tasks.push(handle);
         }
     }
-    for handle in tasks {
-        handle.await.expect("Request failed");
+    let bar = ProgressBar::new(SITES.entries().len() as u64);
+    loop {
+        match rx.try_recv() {
+            #[allow(clippy::unwrap_used)]
+            Ok(value) if value.is_some() => { bar.println(&value.unwrap()); bar.inc(1) },
+            Ok(value) if value.is_none() => { bar.inc(1) }
+            Ok(_) => { }
+            Err(tokio::sync::mpsc::error::TryRecvError::Empty) => { std::thread::sleep(Duration::from_millis(250)); },
+            Err(tokio::sync::mpsc::error::TryRecvError::Disconnected) => { break }
+        }
     }
 }
